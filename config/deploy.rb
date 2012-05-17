4b1e1d9275789ccb37e0339fd9413ed0d7cd8067
Brian Bragg <brian.c.bragg@gmail.com>
2012-05-17 13:22:44 -0400

    small change

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@ require "bundler/capistrano"
 
 server "50.116.57.45", :web, :app, :db, primary: true
 
-set :application, "blog"
+set :application, "gallery"
 set :user, "deployer"
 set :deploy_to, "/home/#{user}/apps/#{application}"
 set :deploy_via, :remote_cache
